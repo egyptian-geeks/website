80c65c23fa8179ada108a10d076a8f8f86fe9e3b
Emad Elsaid <blazeeboy@gmail.com>
2018-01-29 00:28:30 +0100

    display users profile picture and number of posts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def user_avatar(user)
     "http://graph.facebook.com/#{user.fid}/picture?type=square"
   end
+
+  def user_facebook_profile(user)
+    "https://www.facebook.com/#{user.fid}"
+  end
 end

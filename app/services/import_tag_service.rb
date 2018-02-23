62af7f332dbffd806bc55f5830c8b989ce6eece6
AhmedKamal20 <ahmed.kamal200@yahoo.com>
2018-02-24 01:54:18 +0200

    Import Tags

---
+++ app/services/import_tag_service.rb
@@ -0,0 +1,22 @@
+class ImportTagService < ApplicationService
+  def initialize(post)
+   @post = post
+  end
+
+  def call
+    tags = post.message.scan(/(?:\s|^)(?:#(?!\d+(?:\s|$)))(\w+)(?=\s|$)/i).flatten.map(&:downcase)
+
+    unless tags.empty?
+      tags = tags.map { |t| Tag.find_or_create_by(title: t) }
+
+      post.tags << tags
+    end
+
+    return tags
+  end
+
+  private
+
+  attr_reader :post
+
+end
